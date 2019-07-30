status-im/status-mobile

make app usable

src/status_im/hardwallet/core.cljs
@@ -1565,7 +1565,7 @@
                                                             :name name))
                :hardwallet/get-application-info {:pairing    (get-pairing db instance-uid)
                                                  :on-success :hardwallet/save-key-uid-to-account}}
-              (accounts.login/user-login true))))
+              (accounts.login/user-login))))
 
 (fx/defn on-get-keys-error
   [{:keys [db] :as cofx} error]

src/status_im/node/core.cljs
@@ -162,9 +162,7 @@
   (let [network     (if address
                       (get-account-network db address)
                       (:network db))
-        node-config (if (= (:node/on-ready db) :verify-account)
-                      (get-verify-account-config db network)
-                      (get-account-node-config db address))
+        node-config (get-account-node-config db address)
         node-config-json (types/clj->json node-config)]
     (log/info "Node config: " node-config-json)
     {:db        (assoc db

elegant651/status-react

src/status_im/hardwallet/core.cljs
@@ -1657,7 +1657,7 @@
                                                             :name name))
                :hardwallet/get-application-info {:pairing    (get-pairing db instance-uid)
                                                  :on-success :hardwallet/save-key-uid-to-multiaccount}}
-              (multiaccounts.login/user-login true))))
+              (multiaccounts.login/user-login))))
 
 (fx/defn on-get-keys-error
   [{:keys [db] :as cofx} error]

src/status_im/node/core.cljs
@@ -165,9 +165,7 @@
   (let [network     (if address
                       (get-multiaccount-network db address)
                       (:network db))
-        node-config (if (= (:node/on-ready db) :verify-multiaccount)
-                      (get-verify-multiaccount-config db network)
-                      (get-multiaccount-node-config db address))
+        node-config (get-multiaccount-node-config db address)
         node-config-json (types/clj->json node-config)]
     (log/info "Node config: " node-config-json)
     {:db        (assoc db